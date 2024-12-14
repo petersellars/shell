24dfe555fb01b6bb4ffdd546731642617855c6ed
Peter Sellars <psellars@gmail.com>
2024-12-14 16:36:45 +1300

    feat(tests): provide ability to write Shell tests
    * Added shunit2 script for writing tests
    * Creates some example tests to understand how it works
    * Created a test for to learn how to test a function
    * Added a shellcheck directive to ignore sourcing files
    
    Closes: #4

---
+++ utils/adr/test-function.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+log() {
+    local level="$1"
+    local message="$2"
+    local timestamp
+    timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "[$timestamp] [$level] $message"
+}
