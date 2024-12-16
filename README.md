1f7b755803f342688b8e97f2dffcc8c350e1a567
Peter Sellars <psellars@gmail.com>
2024-12-17 00:52:37 +1300

    feat(tests): begin work to create a more testable script
    * refactored and renamed adr installation script
      * refactored main to only run when called, not sourced
      * begun work to make more testable, reducing the global scope
      * add function to create log directory
    * created an initial adr install test script using shunit2
      * add initial test for log directory creation
      * utilised refactored directory function
    * moved shunit2 to the root at this time
    * removed temporary/poc shunit2 code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Shell
 
+[![Built with Devbox](https://www.jetify.com/img/devbox/shield_moon.svg)](https://www.jetify.com/devbox/docs/contributor-quickstart/)
+
 Shell scripts and utilities repository.
--- utils/adr/test-function.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-log() {
-    local level="$1"
-    local message="$2"
-    local timestamp
-    timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-    echo "[$timestamp] [$level] $message"
-}
